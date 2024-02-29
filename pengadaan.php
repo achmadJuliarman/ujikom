fbdd79f382ab7d183f0eb6880a89f0ce1982f708
achmadJuliarman <acmadjuliarman@gmail.com>
2024-02-29 12:33:21 +0700

    update sesuai soal [coba-github-desktop]

--- pengadaan.php
+++ pengadaan.php
@@ -2,20 +2,22 @@
 include_once 'functions.php';
 include_once 'header.php';
 
-$bukuTerdikit = query('SELECT * FROM buku WHERE stok = (SELECT MIN(stok) FROM buku);')[0];
+$bukuTerdikit = query('SELECT * FROM buku WHERE stok = (SELECT MIN(stok) FROM buku);');
  ?> 
 
 <div class="container mt-4 d-flex justify-content-center">
-	<div class="card mb-3" style="width: 20rem;">
+	<?php foreach($bukuTerdikit as $bt) : ?>
+	<div class="card mb-3 mx-2" style="width: 20rem;">
 	  <div class="card-header text-center bg-danger text-white">
 	    Stok Buku Minim
 	  </div>
 	  <ul class="list-group list-group-flush">
-	    <li class="list-group-item">Nama Buku : <?= $bukuTerdikit['nama_buku'] ?></li>
-	    <li class="list-group-item">Pernerbit : <?= $bukuTerdikit['penerbit'] ?></li>
-	    <li class="list-group-item"><b>Stok : <?= $bukuTerdikit['stok'] ?></b></li>
+	    <li class="list-group-item">Nama Buku : <?= $bt['nama_buku'] ?></li>
+	    <li class="list-group-item">Pernerbit : <?= $bt['penerbit'] ?></li>
+	    <li class="list-group-item"><b>Stok : <?= $bt['stok'] ?></b></li>
 	  </ul>	
 	</div>
+	<?php endforeach; ?>
 </div>	
 
 
